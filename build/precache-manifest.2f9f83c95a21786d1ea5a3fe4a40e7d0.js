8cf37d5c9d9add5caba8462f7056e67c9edead15
unknown <Pikachoo28>
2021-02-03 12:26:00 +0530

    first commit

---
+++ build/precache-manifest.2f9f83c95a21786d1ea5a3fe4a40e7d0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "168aaeeb20d6172de5e7c1ba052e7fc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ee5edb533b605efc9f6",
+    "url": "/static/css/2.a35c51d9.chunk.css"
+  },
+  {
+    "revision": "cc77d7ece9c7088ddb02",
+    "url": "/static/css/main.6ac65217.chunk.css"
+  },
+  {
+    "revision": "2ee5edb533b605efc9f6",
+    "url": "/static/js/2.eb038392.chunk.js"
+  },
+  {
+    "revision": "9cfe4d6dde150644f94a4f55febb3274",
+    "url": "/static/js/2.eb038392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc77d7ece9c7088ddb02",
+    "url": "/static/js/main.3304634f.chunk.js"
+  },
+  {
+    "revision": "0980db0307ffad65803b",
+    "url": "/static/js/runtime-main.df5858f9.js"
+  },
+  {
+    "revision": "326aae133010f7579a016d89d68745a2",
+    "url": "/static/media/img1.326aae13.jpg"
+  },
+  {
+    "revision": "973150b8d1e5857457905b0ee4c9178f",
+    "url": "/static/media/signup.973150b8.svg"
+  }
+]);
